b8479921afd6e9b3609eacabf1beeb472bd3844b
mjrb <www.w.com123@gmail.com>
2020-04-15 18:17:04 -0400

    add some modules for separation of concerns

--- backend/cybnetics/endpoints.py
+++ backend/cybnetics/endpoints.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config.from_pyfile('./config.py')
+
+@app.before_first_request
+def db_connect():
+    app.db = MongoClient(app.config['DB_URI']).get_default_database()
 
 @app.route('/test', methods=['GET'])
 def test():
---
+++ backend/cybnetics/resources/model_attacks.py
@@ -0,0 +1,11 @@
+from .db import db
+
+def simulate_attack(attack_image, user):
+    # store attack image
+    # run the model with the image to see if its defeated
+    # delete the image
+    pass
+
+def save_attack(user, points, finish):
+    # add attack to model document in db. indicate points and gold, silver or bronze
+    pass
