5bb37015e53b05054a0d7749fc8582079047de4c
mjrb <www.w.com123@gmail.com>
2020-04-15 20:38:39 -0400

    updated params

--- backend/cybnetics/resources/model_attacks.py
+++ backend/cybnetics/resources/model_attacks.py
@@ -1,11 +1,11 @@
 from .db import db
 
-def simulate_attack(attack_image, user):
+def simulate_attack(model_id, label, attack_image, user):
     # store attack image
     # run the model with the image to see if its defeated
     # delete the image
     pass
 
-def save_attack(user, points, finish):
+def save_attack(model_id, label, user, points, finish):
     # add attack to model document in db. indicate points and gold, silver or bronze
     pass
