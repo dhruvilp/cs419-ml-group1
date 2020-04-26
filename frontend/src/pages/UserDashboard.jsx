efe4cb733e8f0298032e4acced729c490f615b82
DhruvilPatel <atdhruvilpatel@gmail.com>
2020-04-26 11:42:54 -0400

    misc auth logic updates

---
+++ frontend/src/pages/UserDashboard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Card, Container, } from "reactstrap";
+
+import CybneticsNavbar from "components/CybneticsNavbar";
+import CybneticsFooter from "components/CybneticsFooter";
+
+class UserDashboard extends React.Component {
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+  render() {
+    return (
+      <>
+        <CybneticsNavbar />
+        <main className="profile-page" ref="main">
+          <section className="section-cybnetics-cover section-shaped my-0">
+            <div className="shape shape-primary"></div>
+            <div className="separator separator-bottom separator-skew">
+              <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+                <polygon className="fill-white" points="2560 0 2560 100 0 100"/>
+              </svg>
+            </div>
+          </section>
+          <section className="section">
+            <Container>
+              <Card className="card-profile shadow mt--300">
+                <div className="px-4">
+                  <div className="text-center mt-5">
+                    <h3>
+                        User Dashboard
+                    </h3>
+                  </div>
+                  <div className="text-center mt-5">
+                    <Container>
+                      <h6>
+                        Upload Trained ML Models
+                      </h6>
+                    </Container>
+                  </div>
+                </div>
+              </Card>
+            </Container>
+          </section>
+        </main>
+        <CybneticsFooter />
+      </>
+    );
+  }
+}
+
+export default UserDashboard;
