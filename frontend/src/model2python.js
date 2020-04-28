0d3566a2cb558dd4ed0f36f377a39f078a11f407
mjrb <www.w.com123@gmail.com>
2020-04-28 15:31:59 -0400

    bugfix and example call function

--- frontend/src/model2python.js
+++ frontend/src/model2python.js
@@ -1,13 +1,16 @@
-export default const model2python = ({layers, pools, color}) = {
+export const model2python = ({layers, pools, color}) => {
     let result = "";
     let indent = 0;
-    const line = (string) = {
-        for (let i = indent; i > 0; i--) result += '    ';
+    const line = (string) => {
+        for(let i = indent; i > 0; i--) result += '    ';
         result += string + '\n'
     };
     // imports
+    line('import cv2')
+    line('import torch');
     line('import torch.nn as n');
     line('import torch.nn.functional as F');
+
     line('');
     
     // class definition
@@ -20,13 +23,13 @@ export default const model2python = ({layers, pools, color}) = {
     indent++;
     
     // pools
-    pools.foreach(({type, args}, i) => {
+    pools.forEach(({type, args}, i) => {
         line(`self.pool${i} = nn.${type}(${args.join(", ")})`)
     })
     line('');
     
     // layers
-    layers.foreach(({name, type, args}) => {
+    layers.forEach(({name, type, args}) => {
         if (type !== 'view') {
             line(`self.${name} = nn.${type}(${args.join(", ")})`)
         }
@@ -37,7 +40,7 @@ export default const model2python = ({layers, pools, color}) = {
     // forward function
     line('def forward(self, x):');
     indent++;
-    layers.foreach(({name, type, args, pool, activation}) => {
+    layers.forEach(({name, type, args, pool, activation}) => {
         if (type === 'view') {
             line(`x = x.view(x, ${args.join(", ")})`)
         } else {
@@ -59,9 +62,36 @@ export default const model2python = ({layers, pools, color}) = {
     // convert_tensor
     line('def convert_tensor(filename):')
     indent++;
-    line('img = cv2.imread(filename, 0)')
     if (color) {
+        line('img = cv2.imread(filename, cv2.IMREAD_COLOR)');
+        line('init_reshape = img.transpose((2, 0, 1))');
+        line('img_tensor = torch.from_numpy(init_reshape)');
+        line('return img_tensor.float()');
         
     } else {
+        line('img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)');
+        line('init_reshape = img.transpose((2, 0, 1))');
+        line('img_tensor = torch.from_numpy(init_reshape).reshape([1,3, img.shape[0], img.shape[1]])');
+        line('return img_tensor.float()');
     }
+    return result;
+}
+
+export const example_call = () => {
+    return model2python({
+        layers: [
+            {'type': 'Conv2d', 'args': [3, 32, 3], 'activation': 'relu', 'name': 'conv1'},
+            {'type': 'Conv2d', 'args': [32, 32, 3], 'activation': 'relu', 'pool': 0, 'name': 'conv2'},
+            {'type': 'Conv2d', 'args': [32, 128, 3], 'activation': 'relu', 'name': 'conv3'},
+            {'type': 'Conv2d', 'args': [128, 128, 3], 'activation': 'relu', 'pool': 0, 'name': 'conv4'},
+            {'type': 'view', 'args': [-1, 128 * 5 * 5]},
+            {'type': 'Linear', 'args': [128 * 5 * 5, 256], 'activation': 'relu', 'name': 'fc1'},
+            {'type': 'Linear', 'args': [256, 256], 'activation': 'relu', 'name': 'fc2'},
+            {'type': 'Linear', 'args': [256, 10], 'name': 'fc3'}
+        ],
+        pools: [
+            {'type': 'MaxPool2d', 'args': [2, 2]}
+        ],
+        color: true
+    })
 }
