1164725d118442329c1bdafbebb0d0e8cde91ca9
mjrb <www.w.com123@gmail.com>
2020-04-28 17:12:10 -0400

    add dropouts and flatten

--- frontend/src/model2python.js
+++ frontend/src/model2python.js
@@ -1,4 +1,4 @@
-export const model2python = ({layers, pools, color}) => {
+export const model2python = ({layers, pools, dropouts, color}) => {
     let result = "";
     let indent = 0;
     const line = (string) => {
@@ -19,18 +19,28 @@ export const model2python = ({layers, pools, color}) => {
     
     // init function
     line('def __init__(self):')
-    line('super().__init__()')
     indent++;
+    line('super().__init__()')
     
     // pools
-    pools.forEach(({type, args}, i) => {
-        line(`self.pool${i} = nn.${type}(${args.join(", ")})`)
-    })
-    line('');
+    if (pools !== undefined) {
+        pools.forEach(({type, args}, i) => {
+            line(`self.pool${i} = nn.${type}(${args.join(", ")})`)
+        })
+        line('');
+    }
+
+    // dropouts
+    if (dropouts !== undefined) {
+        dropouts.forEach(({type, args}, i) => {
+            line(`self.dropout${i} = nn.${type}(${args.join(", ")})`)
+        })
+        line('');
+    }
     
     // layers
     layers.forEach(({name, type, args}) => {
-        if (type !== 'view') {
+        if (type !== 'view' && type !== 'flatten') {
             line(`self.${name} = nn.${type}(${args.join(", ")})`)
         }
     })
@@ -40,9 +50,11 @@ export const model2python = ({layers, pools, color}) => {
     // forward function
     line('def forward(self, x):');
     indent++;
-    layers.forEach(({name, type, args, pool, activation}) => {
+    layers.forEach(({name, type, args, pool, activation, dropout}) => {
         if (type === 'view') {
             line(`x = x.view(x, ${args.join(", ")})`)
+        } else if (type == 'flatten') {
+            line(`x = torch.flatten(x, ${args.join(", ")})`)
         } else {
             let temp = `self.${name}(x)`;
             if (activation !== undefined) {
@@ -52,6 +64,9 @@ export const model2python = ({layers, pools, color}) => {
                 temp = `self.pool${pool}(${temp})`
             }
             line(`x = ${temp}`);
+            if (dropout !== undefined) {
+                line(`x = self.dropout${dropout}(x)`)
+            }
         }
     })
     line('return x');
@@ -80,18 +95,19 @@ export const model2python = ({layers, pools, color}) => {
 export const example_call = () => {
     return model2python({
         layers: [
-            {'type': 'Conv2d', 'args': [3, 32, 3], 'activation': 'relu', 'name': 'conv1'},
-            {'type': 'Conv2d', 'args': [32, 32, 3], 'activation': 'relu', 'pool': 0, 'name': 'conv2'},
-            {'type': 'Conv2d', 'args': [32, 128, 3], 'activation': 'relu', 'name': 'conv3'},
-            {'type': 'Conv2d', 'args': [128, 128, 3], 'activation': 'relu', 'pool': 0, 'name': 'conv4'},
-            {'type': 'view', 'args': [-1, 128 * 5 * 5]},
-            {'type': 'Linear', 'args': [128 * 5 * 5, 256], 'activation': 'relu', 'name': 'fc1'},
-            {'type': 'Linear', 'args': [256, 256], 'activation': 'relu', 'name': 'fc2'},
-            {'type': 'Linear', 'args': [256, 10], 'name': 'fc3'}
+            {'type': 'Conv2d', 'args': [1, 32, 3], 'activation': 'relu', 'name': 'conv1'},
+            {'type': 'Conv2d', 'args': [32, 64, 3], 'activation': 'relu', 'pool': 0, 'dropout': 0, 'name': 'conv2'},
+            {'type': 'flatten', 'args': [1]},
+            {'type': 'Linear', 'args': [9216, 128], 'activation': 'relu', 'dropout': 1, 'name': 'fc1'},
+            {'type': 'Linear', 'args': [128, 10], 'activation': 'log_softmax', 'name': 'fc2'},
         ],
         pools: [
             {'type': 'MaxPool2d', 'args': [2, 2]}
         ],
-        color: true
+        dropouts: [
+        {'type': 'Dropout2d', 'args': [0.25]},
+        {'type': 'Dropout2d', 'args': [0.5]}
+        ],
+        color: false
     })
 }
